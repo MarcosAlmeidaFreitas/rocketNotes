ec216a8093f5a04f08e3c65470966a5c7661bed0
Marcos Antonio Almeida Freitas <67704150+MarcosAlmeidaFreitas@users.noreply.github.com>
2024-01-10 20:58:27 -0300

    Init Style Global

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,12 @@
 import { Container } from './styles'
 export function Details(){
   return(
-    //para retornar mais de um elemento é necessario usar uma div ou fragment
-    //O fragment não sofre nenhum tipo de estilização ao contrario da div.
-    <Container>
+    //para retornar mais de um elemento é necessário usar uma div ou fragment
+    //O fragment não sofre nenhum tipo de estilização ao contrario da div pode ser importado através do {fragment} from react.
+    //nesse caso ele está usando as configs do container que vem do styles
+    <Container> 
       <h1>Hello World</h1>
       <span>Testando o react</span>
     </Container>
-  )
+  );
 }
